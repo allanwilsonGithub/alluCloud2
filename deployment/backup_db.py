7b9cabd9058cc9d8931659d7a2a60323de0ad326
Allan Wilson <allan@allanwilson.net>
2017-11-11 02:09:23 +0200

    .

--- deployment/backup_db.py
+++ deployment/backup_db.py
@@ -6,4 +6,5 @@ import time
 db_user = 'root'
 db_password = 'neerg42'
 
-os.popen("mysqldump -u %s -p%s eventdb events eventtypes > deployment\db_backups\%s.sql" % (db_user,db_password,"eventsdb1"))
+os.popen("mysqldump -u %s -p%s eventdb events eventtypes > deployment\db_backups\%s.sql" % (db_user,db_password,"eventsdb"))
+os.popen("mysqldump -u %s -p%s booksdb authors categories formats rel_title_author series titles > deployment\db_backups\%s.sql" % (db_user,db_password,"booksdb"))

1ef460fd6404cab4be6f805401a73b06409a9274
Allan Wilson <allan@allanwilson.net>
2017-11-12 00:41:13 +0200

    ,

---
+++ lib/alluCloudUtils.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+def get_MySQLPass:
+    f = open('../alluCloudCredentials.ini', 'r')
+    lines = f.readlines()
+    for line in lines:
+        if "MySQL password" in line:
+            MySQLPass = line.split(":")[1]
+            return MySQLPass
+
+def get_HomeBackupZipPass:
+    f = open('../alluCloudCredentials.ini', 'r')
+    lines = f.readlines()
+    for line in lines:
+        if "Home Backup ZIP password" in line:
+            HomeBackupZipPass = line.split(":")[1]
+            return HomeBackupZipPass
